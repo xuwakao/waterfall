c99cddebaac03f82bc8608551e59200288f96441
xuwakao <xuwakao@gmail.com>
2014-08-10 03:06:18 +0800

    test framework

--- src/cn/wacao/waterfall/MainActivity.java
+++ src/cn/wacao/waterfall/MainActivity.java
@@ -2,6 +2,8 @@ package cn.wacao.waterfall;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import cn.wacao.waterfall.framework.test.TestFramework;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +13,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView textView = (TextView) findViewById(R.id.test);
+        textView.setText(TestFramework.getTestHello());
     }
 }
---
+++ waterfall-framework/src/cn/wacao/waterfall/framework/test/TestFramework.java
@@ -0,0 +1,10 @@
+package cn.wacao.waterfall.framework.test;
+
+/**
+ * Created by xujiexing on 2014/8/10.
+ */
+public class TestFramework {
+    public static String getTestHello() {
+        return "Hello, i am testing";
+    }
+}
